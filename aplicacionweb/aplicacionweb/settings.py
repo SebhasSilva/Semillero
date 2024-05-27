f4f4fd4e059b50fc3f7411b7c557857d1810c46d
Sebas <sebhas.silva@gmail.com>
2024-05-26 22:39:54 -0500

    Se ha agregado linea 40 y 140 en settings de aplicacionweb

--- aplicacionweb/aplicacionweb/settings.py
+++ aplicacionweb/aplicacionweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'users',
     'rest_framework',
     'photos',
@@ -136,3 +137,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SITE_ID = 1
