a94981f547908a88d24ebf2eb20e8213626c98ce
Sebas <sebhas.silva@gmail.com>
2024-05-26 22:04:03 -0500

    Se ha nmodificado views.py

--- aplicacionweb/photos/views.py
+++ aplicacionweb/photos/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Photo
+from .serializers import PhotoSerializer
 
-# Create your views here.
+class PhotoUploadView(generics.CreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PhotoListView(generics.ListAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
