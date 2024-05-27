474cbb3413a3ac3723c294113694c698cead3ff9
Sebas <sebhas.silva@gmail.com>
2024-05-27 08:22:39 -0500

    se ha modificado models de photos

--- aplicacionweb/photos/models.py
+++ aplicacionweb/photos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Photo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
