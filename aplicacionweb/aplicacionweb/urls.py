b866c02e500b05704d88748e9a43f1e5876326de
Sebas <sebhas.silva@gmail.com>
2024-05-26 22:16:54 -0500

    Se modifica urls de aplicacionweb

--- aplicacionweb/aplicacionweb/urls.py
+++ aplicacionweb/aplicacionweb/urls.py
@@ -18,4 +18,13 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/photos/', include('photos.urls')),
 ]
+
+# Para servir archivos de media en desarrollo
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
