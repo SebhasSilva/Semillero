f38659e89c07cc843801b3ccbcd334e0eca5ee50
Sebas <sebhas.silva@gmail.com>
2024-06-04 08:29:33 -0500

    Se Definen las rutas para las vistas de autenticación

--- aplicacionweb/users/urls.py
+++ aplicacionweb/users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import RegisterView, CustomTokenObtainPairView
+from django.contrib.auth import views as auth_views
+from .views import register, login_view
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
