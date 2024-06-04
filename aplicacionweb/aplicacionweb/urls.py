547def8ef7ebecaf2265f95ce111a0735e080c8a
Sebas <sebhas.silva@gmail.com>
2024-06-03 21:57:25 -0500

    Se ha modificado urls de aplicacionweb

--- aplicacionweb/aplicacionweb/urls.py
+++ aplicacionweb/aplicacionweb/urls.py
@@ -13,18 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/photos/', include('photos.urls')),
+    path('users/', include('users.urls')),
+    path('photos/', include('photos.urls')),
 ]
-
-# Para servir archivos de media en desarrollo
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
