e64ae6773ccd21d3600f8b698107fbebd49223db
Sebas <sebhas.silva@gmail.com>
2024-06-04 08:02:00 -0500

    se ha agregado el codigo para las siguientes vistas para registro, inicio de sesión y recuperación de contraseña

--- aplicacionweb/users/views.py
+++ aplicacionweb/users/views.py
@@ -1,15 +1,34 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from django.contrib.auth import get_user_model
-from .serializers import CustomUserSerializer, CustomTokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
-User = get_user_model()
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = CustomUserSerializer
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('photo_upload')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
