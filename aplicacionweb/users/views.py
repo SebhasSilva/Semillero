4e9806d8876c00a596c4b0b0f5e95b413e2f92e8
Sebas <sebhas.silva@gmail.com>
2024-06-04 21:34:24 -0500

    se han realizado varias modificaciones

--- aplicacionweb/users/views.py
+++ aplicacionweb/users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
 from django.shortcuts import render, redirect
@@ -13,8 +13,9 @@ def register(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('login')
+            if user is not None:
+                login(request, user)
+                return redirect('login')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
@@ -32,3 +33,24 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'users/reset_password.html'
+    success_url = reverse_lazy('password_reset_done')
+    email_template_name = 'users/password_reset_email.html'
+    form_class = PasswordResetForm
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'users/password_reset_sent.html'
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_complete')
+    form_class = SetPasswordForm
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'users/password_reset_complete.html'
