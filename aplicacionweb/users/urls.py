505644738c5de9b99a6d6d58335368d12f576909
Sebas <sebhas.silva@gmail.com>
2024-06-05 15:32:36 -0500

    Se ha modificado el codigo de urls.py

--- aplicacionweb/users/urls.py
+++ aplicacionweb/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register, login_view
+from .views import register, login_view, logout_view, CustomPasswordResetView, CustomPasswordResetDoneView, CustomPasswordResetConfirmView, CustomPasswordResetCompleteView
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='users/reset_password.html'), name='password_reset'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_sent.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('logout/', logout_view, name='logout'),
+    path('reset_password/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_sent/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
