88f8dd1f76e76cc51b2d0c1acce3d1ce6c937646
Sebas <sebhas.silva@gmail.com>
2024-06-06 07:49:53 -0500

    Se ha agregado lineas de codigo en urls.py de la aplicacionweb

--- aplicacionweb/aplicacionweb/urls.py
+++ aplicacionweb/aplicacionweb/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from users.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('photos/', include('photos.urls')),
-] 
+    path('', home, name='home'), #Ruta pagina de inicio
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
