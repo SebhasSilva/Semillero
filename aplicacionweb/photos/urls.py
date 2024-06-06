471b48916556e0c6bc6f459ddacee48e3dcbd2b1
Sebas <sebhas.silva@gmail.com>
2024-06-05 21:40:45 -0500

    Se ha mejorado el codigo de urls.py de photos

--- aplicacionweb/photos/urls.py
+++ aplicacionweb/photos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PhotoUploadView, PhotoListView
+from .views import upload_photos, PhotoUploadView, PhotoListView
 
 urlpatterns = [
-    path('upload/', PhotoUploadView.as_view(), name='photo_upload'),
-    path('', PhotoListView.as_view(), name='photo_list'),
+    path('upload/', PhotoUploadView.as_view(), name='photo_upload_api'),  # Para la API de subida
+    path('list/', PhotoListView.as_view(), name='photo_list'),  # Para listar las fotos
+    path('upload/ajax/', upload_photos, name='upload_photos'),  # Para la subida vía Ajax
 ]
