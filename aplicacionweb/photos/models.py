4214bf4843d43c5b06871807d6552a66fd7b4080
Sebas <sebhas.silva@gmail.com>
2024-06-06 07:38:08 -0500

    Se han realizado migraciones y cambios en models.py de photos

--- aplicacionweb/photos/models.py
+++ aplicacionweb/photos/models.py
@@ -1,11 +1,10 @@
-from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.db import models
 
 class Photo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='photos/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username} - {self.uploaded_at}'
+        return f'{self.user.username} - {self.image.name}'
