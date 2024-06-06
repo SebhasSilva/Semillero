6f70735dd75f410726afbe8bc052c5efdd7fee78
Sebas <sebhas.silva@gmail.com>
2024-06-05 21:34:14 -0500

    Se ha mejorado el codigo de views.py en photos

--- aplicacionweb/photos/views.py
+++ aplicacionweb/photos/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Photo
@@ -18,3 +21,19 @@ class PhotoListView(generics.ListAPIView):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+@login_required
+def upload_photos(request):
+    if request.method == 'POST':
+        photos = request.FILES.getlist('photos')
+        photo_urls = []
+
+        for photo in photos:
+            photo_instance = Photo.objects.create(user=request.user, image=photo)
+            photo_urls.append({
+                'url': photo_instance.image.url
+            })
+
+        return JsonResponse({'success': True, 'photos': photo_urls})
+
+    return JsonResponse({'success': False})
