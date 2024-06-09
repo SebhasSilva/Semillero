d26c72207e3ae50e3bfd0d70f5d734e9fa64c93c
Sebas <sebhas.silva@gmail.com>
2024-06-08 19:36:19 -0500

    Funcionamiento, pendiente corregir registro

--- aplicacionweb/users/forms.py
+++ aplicacionweb/users/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email address.')
+    contact_number = forms.CharField(max_length=15, required=True, help_text='Required.')
+    birth_date = forms.DateField(required=True, help_text='Required. Format: YYYY-MM-DD')
+    address = forms.CharField(max_length=255, required=True, help_text='Required.')
+    city = forms.CharField(max_length=100, required=True, help_text='Required.')
+
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'contact_number', 'birth_date', 'address', 'city', 'password1', 'password2')
