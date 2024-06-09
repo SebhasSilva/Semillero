8ba3e48865a342de6983155ad101f72d62d353a9
Sebas <sebhas.silva@gmail.com>
2024-06-08 19:36:19 -0500

    Funcionamiento, pendiente corregir registro

--- aplicacionweb/aplicacionweb/urls.py
+++ aplicacionweb/aplicacionweb/urls.py
@@ -17,14 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from users.views import home
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('photos/', include('photos.urls')),
-    path('', home, name='home'), #Ruta pagina de inicio
-] 
+    path('', user_views.home, name='home'),  # Ruta principal
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
