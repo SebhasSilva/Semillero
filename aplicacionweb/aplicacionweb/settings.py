679213d0971ff61894b0bcc68535bb6342ece430
Sebas <sebhas.silva@gmail.com>
2024-06-10 18:46:57 -0500

    Formulario mejorado pero falta ajustarlo para que sea responsivo a las pantallas

--- aplicacionweb/aplicacionweb/settings.py
+++ aplicacionweb/aplicacionweb/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    Path(BASE_DIR).joinpath('static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
