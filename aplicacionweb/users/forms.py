ad947f62db8869a5684d68140ece230894f09021
Sebas <sebhas.silva@gmail.com>
2024-06-10 18:46:57 -0500

    Formulario mejorado pero falta ajustarlo para que sea responsivo a las pantallas

--- aplicacionweb/users/forms.py
+++ aplicacionweb/users/forms.py
@@ -3,14 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email address.')
-    contact_number = forms.CharField(max_length=15, required=True, help_text='Required.')
-    birth_date = forms.DateField(required=True, help_text='Required. Format: YYYY-MM-DD')
-    address = forms.CharField(max_length=255, required=True, help_text='Required.')
-    city = forms.CharField(max_length=100, required=True, help_text='Required.')
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=15, required=True)
+    birth_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=True)
+    address = forms.CharField(max_length=255, required=True)
+    city = forms.CharField(max_length=100, required=True)
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'email', 'contact_number', 'birth_date', 'address', 'city', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'email', 'phone_number', 'birth_date', 'address', 'city', 'password1', 'password2')
