fa4538b36bff87580913e22d0b572f8e0c9f2884
Sebas <sebhas.silva@gmail.com>
2024-06-11 19:17:23 -0500

    Correcciones en base.html y models.py de users

--- aplicacionweb/users/models.py
+++ aplicacionweb/users/models.py
@@ -1,5 +1,33 @@
+# users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import random
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    # Añadir otros campos necesarios
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not hasattr(self, 'profile'):
+            Profile.objects.create(user=self)
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    id_number = models.CharField(max_length=6, unique=True, blank=True)
+    photos = models.ManyToManyField('Photo')
+
+    def save(self, *args, **kwargs):
+        if not self.id_number:
+            self.id_number = self.generate_unique_id()
+        super().save(*args, **kwargs)
+
+    def generate_unique_id(self):
+        while True:
+            id_number = ''.join(random.choices('123456789', k=6))
+            if not Profile.objects.filter(id_number=id_number).exists():
+                return id_number
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
