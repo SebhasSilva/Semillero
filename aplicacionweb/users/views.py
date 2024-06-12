0fb382a997aa145422142ea2fbc3ee6bdf4b4434
Sebas <sebhas.silva@gmail.com>
2024-06-11 20:08:19 -0500

    Se ha modificado el views.py de users

--- aplicacionweb/users/views.py
+++ aplicacionweb/users/views.py
@@ -1,3 +1,5 @@
+# users/views.py
+
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
@@ -5,15 +7,17 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
 
+# Vista para la página de inicio
 def home(request):
     return render(request, 'users/home.html')
 
+# Vista para el registro de usuarios
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+            user = form.save()  # Guarda el usuario
+            username = form.cleaned_data.get('email')  # Cambiado a 'email' ya que el formulario utiliza el email para autenticación
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             if user is not None:
@@ -23,6 +27,7 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
+# Vista para el login de usuarios
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -37,23 +42,28 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
 
+# Vista para el logout de usuarios
 def logout_view(request):
     logout(request)
     return redirect('login')
 
+# Vista personalizada para el reseteo de contraseña
 class CustomPasswordResetView(PasswordResetView):
     template_name = 'users/reset_password.html'
     success_url = reverse_lazy('password_reset_done')
     email_template_name = 'users/password_reset_email.html'
     form_class = PasswordResetForm
 
+# Vista personalizada para la confirmación de envío de email para reseteo de contraseña
 class CustomPasswordResetDoneView(PasswordResetDoneView):
     template_name = 'users/password_reset_sent.html'
 
+# Vista personalizada para la confirmación del reseteo de contraseña
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'users/password_reset_confirm.html'
     success_url = reverse_lazy('password_reset_complete')
     form_class = SetPasswordForm
 
+# Vista personalizada para la finalización del reseteo de contraseña
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
     template_name = 'users/password_reset_complete.html'
