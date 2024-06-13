504508db9af2f5150a9eb8e7ce87ecc739e864ec
Sebas <sebhas.silva@gmail.com>
2024-06-13 17:06:57 -0500

    Funcionando

--- aplicacionweb/photos/urls.py
+++ aplicacionweb/photos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import upload_photos, PhotoUploadView, PhotoListView
+from .views import upload_photos, PhotoUploadView, PhotoListView, PhotoDeleteView
 
 urlpatterns = [
     path('upload/', PhotoUploadView.as_view(), name='photo_upload_api'),  # Para la API de subida
     path('list/', PhotoListView.as_view(), name='photo_list'),  # Para listar las fotos
     path('upload/ajax/', upload_photos, name='upload_photos'),  # Para la subida vía Ajax
+    path('delete/<int:pk>/', PhotoDeleteView.as_view(), name='delete_photo'),  # Para eliminar una foto
 ]
