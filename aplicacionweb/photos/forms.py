07ff64829a45c68fcdeb7b2cd044ff69ba454b4c
Sebas <sebhas.silva@gmail.com>
2024-06-22 08:11:46 -0500

    Funcionando

--- aplicacionweb/photos/forms.py
+++ aplicacionweb/photos/forms.py
@@ -1,9 +1,7 @@
-# photos/forms.py
-
 from django import forms
 from .models import Photo
 
 class PhotoUploadForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ['image']  # Ajusta los campos según tus necesidades
+        fields = ['image']
--- aplicacionweb/photos/models.py
+++ aplicacionweb/photos/models.py
@@ -3,8 +3,14 @@ from django.db import models
 
 class Photo(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='photos')
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE, related_name='profile_photos')
     image = models.ImageField(upload_to='photos/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    visible = models.BooleanField(default=True)  # Agregar el campo visible
 
     def __str__(self):
         return f'{self.user.username} - {self.image.name}'
+
+    @property
+    def id_number(self):
+        return self.profile.id_number
