d511866c6d5e02ecc26bf2f8b020af6f103dff1c
Sebas <sebhas.silva@gmail.com>
2024-10-15 14:04:09 -0500

    final

--- aplicacionweb/aplicacionweb/urls.py
+++ aplicacionweb/aplicacionweb/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('users/', include('users.urls')),  # Incluir las URLs de la aplicación de usuarios
     path('photos/', include('photos.urls')),  # Incluir las URLs de la aplicación de fotos
     path('', user_views.home, name='home'),  # Ruta principal que apunta a la vista home de los usuarios
-    path('estadisticas/', user_views.estadisticas_view, name='estadisticas'),
+    path('estadisticas/', user_views.estadisticas_view, name='estadisticas'), #Cargue de la estadistic
 ]
 
 # Si está en modo DEBUG, servir archivos estáticos y de medios desde el directorio del proyecto
